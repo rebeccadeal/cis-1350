49527d582c940be89e31861ec9ee33c1836582d7
Rebecca Deal <rebeccadeal@me.com>
2017-11-06 15:58:28 -0500

    Complete html09 tutorial & add link to index.html

--- html09/tutorial/scripts/tny_script.js
+++ html09/tutorial/scripts/tny_script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 New Perspectives on HTML5 and CSS3, 7th Edition
 Tutorial 9
@@ -13,3 +15,37 @@ Date: 11/2/17
 Filename: tny_script.js
 Path: /rebecca.deal/html09/tutorial/scripts/tny_script.js
 */
+
+/* Execute the funtion to run and display the countdown clock */
+runClock();
+setInterval("runClock()", 1000);
+
+/* Function to create and run the countdown clock */
+function runClock() {
+  /* Store the current date and time */
+  var currentDay = new Date();
+  var dateStr = currentDay.toLocaleDateString();
+  var timeStr = currentDay.toLocaleTimeString();
+
+  /* Display the current date and time */
+  document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+
+  /* Calculate the days until January 1st */
+  var newYear = new Date("January 1, 2018");
+  var nextYear = currentDay.getFullYear() + 1;
+  newYear.setFullYear(nextYear);
+  var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+
+  /* Calculate the hours left in the current day */
+  var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+
+  /* Calculate the minutes and seconds left in the current hour */
+  var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+  var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+
+  /* Display the time left until New Year's Eve */
+  document.getElementById("days").textContent = Math.floor(daysLeft);
+  document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+  document.getElementById("mins").textContent = Math.floor(minsLeft);
+  document.getElementById("secs").textContent = Math.floor(secsLeft);
+}
